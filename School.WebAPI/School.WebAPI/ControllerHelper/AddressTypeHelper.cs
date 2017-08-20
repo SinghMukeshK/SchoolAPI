3654324349fd1a9e8ad9041c306b3b1ce9c66bda
SinghMukeshK <mukesh06081990@gmail.com>
2017-08-20 17:55:44 +0530

    Added Data Layer and API Controller

---
+++ School.WebAPI/School.WebAPI/ControllerHelper/AddressTypeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using School.DataAccessLayer;
+namespace School.WebAPI.ControllerHelper
+{
+    public class AddressTypeHelper
+    {
+        SchoolContext _dbContext = new SchoolContext();
+
+        public List<AddressType> GetAddressType()
+        {
+            return _dbContext.AddressTypes.ToList();
+        }
+    }
+}
---
+++ School.WebAPI/School.WebAPI/Controllers/APIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using School.WebAPI.ControllerHelper;
+using School.DataAccessLayer;
+
+namespace School.WebAPI.Controllers
+{
+    public class APIController : ApiController
+    {
+        private AddressTypeHelper _addressTypeHelper;
+        public APIController()
+        {
+            _addressTypeHelper = new AddressTypeHelper();
+        }
+
+        [Route("api/GetAddressTypes")]
+        public List<AddressType> GetAddressTypes()
+        {
+            return _addressTypeHelper.GetAddressType();
+        }
+    }
+}
